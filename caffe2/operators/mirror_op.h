6b86eb24e3f970edbdbdbf6c4c0d6f71f4610506
davinwang <davin.wang@live.com>
2017-08-08 18:18:57 +0800

    refer to transpose

--- caffe2/operators/mirror_op.h
+++ caffe2/operators/mirror_op.h
@@ -1,35 +1,48 @@
 #ifndef CAFFE2_OPERATORS_MIRROR_OP_H_
 #define CAFFE2_OPERATORS_MIRROR_OP_H_
 
-#include "caffe2/core/common_omp.h"
 #include "caffe2/core/context.h"
-#include "caffe2/core/logging.h"
 #include "caffe2/core/operator.h"
 
 namespace caffe2 {
 
-template <typename T, class Context>
-class MirrorOp final : public Operator<Context> {
- public:
-  USE_SIMPLE_CTOR_DTOR(MirrorOp);
-  USE_OPERATOR_CONTEXT_FUNCTIONS;
+	template <class Context>
+	class MirrorOp final : public Operator<Context> {
+	public:
+		USE_OPERATOR_CONTEXT_FUNCTIONS;
+		USE_DISPATCH_HELPER;
+		MirrorOp(const OperatorDef& operator_def, Workspace* ws)
+			: Operator<Context>(operator_def, ws),
+			axes_(OperatorBase::GetRepeatedArgument<int>("axes")) {
+			// We will check the legality of axes_: it should be monotone increasing tuple between 0 and X.ndim().
+			for (int i = 1; i < axes_.size(); ++i) {
+				if (axes_[i] != axes_[0] + i) {
+					CAFFE_THROW("Axes should be a monotone increasing tuple between 0 and ndim.");
+				}
+			}
+		}
+		~MirrorOp() {}
 
-  bool RunOnDevice() override;
+		bool RunOnDevice() override {
+			const auto& X = Input(0);
+			auto* Y = Output(0);
+			Y->ResizeLike(X);
+			// Do the actual mirror, which is implemented in DoRunWithType().
+			return DispatchHelper<TensorTypes<float, double, int, long>>::call(
+				this, Input(0));
+		}
 
- protected:
-};
+	protected:
+		template <typename T>
+		bool DoRunWithType();
 
-template <typename T, class Context>
-class MirrorGradientOp final : public Operator<Context> {
- public:
-  USE_SIMPLE_CTOR_DTOR(MirrorGradientOp);
-  USE_OPERATOR_CONTEXT_FUNCTIONS;
-
-  bool RunOnDevice() override;
-
- protected:
-  // Input: Y, dY; Output: dX
-};
+		std::vector<int> axes_;
+		std::vector<TIndex> new_dims_;
+		// buffer_ is used in MirrorOp<CUDAContext> so we can obtain a consistent
+		// buffer on the GPU. It is not used in the CPUContext implementation.
+		Tensor<Context> buffer_;
+		TensorCPU buffer_cpu_;
+	};
 
 } // namespace caffe2
 
