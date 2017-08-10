1a4a7f251ff722e8d5591ba28cbc1478b23a995f
Davin Wang <davin.wang@live.com>
2017-08-10 23:59:21 +0800

    rename to flip

--- caffe2/operators/mirror_op.h
+++ caffe2/operators/mirror_op.h
@@ -1,5 +1,5 @@
-#ifndef CAFFE2_OPERATORS_MIRROR_OP_H_
-#define CAFFE2_OPERATORS_MIRROR_OP_H_
+#ifndef CAFFE2_OPERATORS_FLIP_OP_H_
+#define CAFFE2_OPERATORS_FLIP_OP_H_
 
 #include "caffe2/core/context.h"
 #include "caffe2/core/operator.h"
@@ -7,11 +7,11 @@
 namespace caffe2 {
 
   template <class Context>
-  class MirrorOp final : public Operator<Context> {
+  class FlipOp final : public Operator<Context> {
   public:
     USE_OPERATOR_CONTEXT_FUNCTIONS;
     USE_DISPATCH_HELPER;
-    MirrorOp(const OperatorDef& operator_def, Workspace* ws)
+    FlipOp(const OperatorDef& operator_def, Workspace* ws)
       : Operator<Context>(operator_def, ws),
       axes_(OperatorBase::GetRepeatedArgument<int>("axes")) {
       // We will check the legality of axes_: it should be monotone increasing tuple between 0 and X.ndim().
@@ -21,13 +21,13 @@ namespace caffe2 {
         }
       }
     }
-    ~MirrorOp() {}
+    ~FlipOp() {}
 
     bool RunOnDevice() override {
       const auto& X = Input(0);
       auto* Y = Output(0);
       Y->ResizeLike(X);
-      // Do the actual mirror, which is implemented in DoRunWithType().
+      // Do the actual flip, which is implemented in DoRunWithType().
       return DispatchHelper<TensorTypes<float, double, int, long>>::call(
         this, Input(0));
     }
@@ -37,7 +37,7 @@ namespace caffe2 {
     bool DoRunWithType();
 
     std::vector<int> axes_;
-    // buffer_ is used in MirrorOp<CUDAContext> so we can obtain a consistent
+    // buffer_ is used in FlipOp<CUDAContext> so we can obtain a consistent
     // buffer on the GPU. It is not used in the CPUContext implementation.
     Tensor<Context> buffer_;
     TensorCPU buffer_cpu_;
@@ -45,4 +45,4 @@ namespace caffe2 {
 
 } // namespace caffe2
 
-#endif // CAFFE2_OPERATORS_MIRROR_OP_H_
+#endif // CAFFE2_OPERATORS_FLIP_OP_H_
