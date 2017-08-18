e95b9ed3ba7fea345d417646718afd0682852367
Davin Wang <davin.wang@live.com>
2017-08-18 14:22:26 +0800

    Update flip_op_test.py

--- caffe2/python/operator_test/flip_op_test.py
+++ caffe2/python/operator_test/flip_op_test.py
@@ -7,7 +7,6 @@ from caffe2.python import core
 from hypothesis import given
 import hypothesis.strategies as st
 import caffe2.python.hypothesis_test_util as hu
-import caffe2.python.mkl_test_util as mu
 import numpy as np
 
 import unittest
@@ -15,18 +14,18 @@ import unittest
 
 class TestFlip(hu.HypothesisTestCase):
 
-    @given(H=st.sampled_from([19]),
-           W=st.sampled_from([19]),
-           engine=st.sampled_from([None, "CUDNN"]),
+    @given(H=st.sampled_from([1,3,8]),
+           W=st.sampled_from([2,5,11]),
+           engine=st.sampled_from([None]),
            **hu.gcs)
     def test_flip(self, H, W, engine, gc, dc):
         X = np.random.rand(H, W).astype(np.float32)
-        op = core.CreateOperator("Flip", ["X"], ["Y"], axes=(1), engine=engine)
+        op = core.CreateOperator("Flip", ["X"], ["Y"], axes=(1,), engine=engine)
         self.assertReferenceChecks(
             device_option=gc,
             op=op,
             inputs=[X],
-            reference=np.flip,
+            reference=np.fliplr,
         )
 
 
